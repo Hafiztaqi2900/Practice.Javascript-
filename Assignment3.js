c93b5c2d84ced90a9db39377e3ff446f20fdb073
Hafiz Muhammad Taqi <hafiztaqi1829@gmail.com>
2024-03-06 00:21:07 +0500

    Assignment 3 complete

--- Assignment3.js
+++ Assignment3.js
@@ -2,7 +2,8 @@ alert("I am 17 years old")
 
 alert("You have visited this site 14 times")
 
-document.write("My birth year is 1990 \n Data type of my declared variable is number")
+document.write("My birth year is 1990 </br> Data type of my declared variable is number </br>")
 
-// var clothes= 
-// document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store")
+var clothes = ("</br>John Doe ordered 5 T-shirt(s) on XYZ Clothing store")
+
+document.write(clothes)
