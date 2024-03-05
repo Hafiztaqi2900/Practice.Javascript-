222380c6f0d0670f79af5a8607c04033da572546
Hafiz Muhammad Taqi <hafiztaqi1829@gmail.com>
2024-03-06 00:10:46 +0500

    Assignment 3

---
+++ Assignment3.js
@@ -0,0 +1,8 @@
+alert("I am 17 years old")
+
+alert("You have visited this site 14 times")
+
+document.write("My birth year is 1990 \n Data type of my declared variable is number")
+
+// var clothes= 
+// document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store")
