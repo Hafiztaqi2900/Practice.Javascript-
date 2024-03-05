07fb67651b319314a4bac8a6bb8141d4c56965c6
Hafiz Muhammad Taqi <hafiztaqi1829@gmail.com>
2024-03-05 23:25:43 +0500

    Assignment 2

---
+++ Assignment2.js
@@ -0,0 +1,11 @@
+var Username = prompt("What your name");
+ if (Username==="Taqi") {
+     alert("Correct");
+ }
+var age = prompt("What your age");
+ if (age==="17") {
+     alert("Correct");
+ }
+alert("Certified Mobile Application Development")
+
+alert("Pizza")
